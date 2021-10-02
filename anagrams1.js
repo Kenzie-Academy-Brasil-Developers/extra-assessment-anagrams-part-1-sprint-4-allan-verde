f56187c9d55745a33a2bb2ea1331ca7a29fe32a4
Allan Verde <allangreen2906@gmail.com>
2021-10-02 15:00:15 -0300

    Concluindo entrega primeira versao

--- anagrams1.js
+++ anagrams1.js
@@ -1,14 +1,46 @@
-const button = document.getElementById("findButton");
+const button = document.getElementById("findButton")
 button.addEventListener("click", function () {
-    let typedText = document.getElementById("input").value;
-    getAnagramsOf(typedText);
+    let typedText = document.getElementById("input").value
 
-    function alphabetize(wold) {
-        return wold.toLowerCase().split("").sort().join("").trim();
-    }
-    textContent = alphabetize(typedText)
+    let arrayAnagramas = getAnagramsOf(typedText)
     
-    document.body.appendChild()
-    
+    console.log(arrayAnagramas) 
+
+    contrutorHTML(arrayAnagramas)
+
+    return arrayAnagramas
 });
   
+function getAnagramsOf(input) {
+    let inputOrdenado = input.split('').sort().join('').trim()
+    let arrayPalavrasAnagrama = []
+
+    for (let palavraOrdenada of words) {
+        let palavraComparadaOrdenada = palavraOrdenada.split('').sort().join('').trim()
+        
+        if (palavraComparadaOrdenada === inputOrdenado) {
+            arrayPalavrasAnagrama.push(palavraOrdenada)
+        }
+    }
+
+    if (arrayPalavrasAnagrama.length === 0) {
+        arrayPalavrasAnagrama.push('Nenhum')
+    }
+
+    return arrayPalavrasAnagrama
+}
+
+const secao = document.getElementsByTagName('section')[0]
+const blocoResultado = document.createElement('div')
+blocoResultado.classList.add('blocoResultado')
+
+function contrutorHTML(arrayPalavras) {
+    secao.appendChild(blocoResultado)
+
+    blocoResultado.innerText = ''
+
+    const blocoResultadoTexto = document.createElement('p')
+    blocoResultadoTexto.classList.add('blocoResultado__texto')
+    blocoResultado.appendChild(blocoResultadoTexto)
+    blocoResultadoTexto.innerHTML = `O resultado encontrado é: <br/> <span>${arrayPalavras}</span>`
+}
